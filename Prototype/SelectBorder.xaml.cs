1845c415878e0481f60c9206d7b10d8f4003cb9e
Kamesuta <kamesuta@gmail.com>
2023-09-18 21:04:32 +0900

    NativeMethodsを改良

--- Prototype/SelectBorder.xaml.cs
+++ Prototype/SelectBorder.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Threading;
 
 namespace screen_draw_together.Prototype
 {
@@ -11,9 +14,47 @@ namespace screen_draw_together.Prototype
     /// </summary>
     public partial class SelectBorder : Window
     {
+        /// <summary>
+        /// 範囲が選択された時
+        /// 右クリックでキャンセルされた場合はnullが渡される
+        /// </summary>
+        public event Action<Rect?> OnRectConfirmed = delegate { };
+
+        // タイマーのインスタンス
+        private DispatcherTimer _timer = new()
+        {
+            // インターバルを設定
+            Interval = TimeSpan.FromMilliseconds(10)
+        };
+        // 現在の範囲
+        private Rect? _rect;
+
         public SelectBorder()
         {
             InitializeComponent();
+            // タイマーメソッドを設定
+            _timer.Tick += Timer_Tick;
+            // タイマーを開始
+            _timer.Start();
+            // ウィンドウの大きさを全画面を覆う範囲にする
+            SetWindowSizeToOverlay();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // ウィンドウが閉じられるときに、タイマーを停止
+            _timer.Stop();
+        }
+
+        /// <summary>
+        /// オーバーレイ用にウィンドウの大きさを全画面を覆う範囲にする
+        /// </summary>
+        public void SetWindowSizeToOverlay()
+        {
+            Left = SystemParameters.VirtualScreenLeft;
+            Top = SystemParameters.VirtualScreenTop;
+            Width = SystemParameters.VirtualScreenWidth;
+            Height = SystemParameters.VirtualScreenHeight;
         }
 
         /// <summary>
@@ -23,16 +64,141 @@ namespace screen_draw_together.Prototype
         /// <see cref="https://stackoverflow.com/a/51388226"/>
         public void SetRect(Rect rect)
         {
+            // アニメーションを設定
             var myRectAnimation = new RectAnimation
             {
                 Duration = TimeSpan.FromSeconds(0.1),
             };
 
+            // アニメーションを開始
             SelectRect.BeginAnimation(RectangleGeometry.RectProperty, myRectAnimation);
             SelectRectBorder.BeginAnimation(RectangleGeometry.RectProperty, myRectAnimation);
 
+            // 範囲を更新
             SelectRect.Rect = rect;
             SelectRectBorder.Rect = rect;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                // 左クリックで範囲を確定
+                OnRectConfirmed(_rect);
+            }
+            else if (e.RightButton == MouseButtonState.Pressed)
+            {
+                // 右クリックでキャンセル
+                OnRectConfirmed(null);
+            }
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            // カーソルの位置にあるウィンドウの範囲を取得
+            var rect = WindowNativeMethods.GetWindowRectOnCursor();
+            // 取得できなかったら何もしない、更新されていない場合も何もしない
+            if (rect is null || rect == _rect) return;
+            _rect = rect;
+
+            // 範囲を更新
+            SetRect(rect.Value);
+        }
+
+        /// <summary>
+        /// P/Invokeクラス
+        /// </summary>
+        internal partial class WindowNativeMethods
+        {
+            private const int WS_CHILD = 0x40000000;
+            private const int GWL_STYLE = -16;
+            private const int GA_ROOT = 2;
+            private const int DWMWA_EXTENDED_FRAME_BOUNDS = 9;
+
+            [LibraryImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static partial bool GetCursorPos(out System.Drawing.Point p);
+
+            [LibraryImport("user32.dll")]
+            private static partial IntPtr WindowFromPoint(System.Drawing.Point p);
+
+            [LibraryImport("user32.dll")]
+            private static partial long GetWindowLongA(IntPtr hWnd, int nIndex);
+
+            [LibraryImport("user32.dll")]
+            private static partial IntPtr GetAncestor(IntPtr hWnd, uint gaFlags);
+
+            [LibraryImport("user32.dll")]
+            private static partial IntPtr GetShellWindow();
+
+            [LibraryImport("user32.dll")]
+            private static partial IntPtr FindWindowA([MarshalAs(UnmanagedType.LPStr)] string? lpClassName, [MarshalAs(UnmanagedType.LPStr)] string? lpWindowName);
+
+            /// <summary>
+            /// 引数のcbAttributeにこれを渡す
+            /// </summary>
+            /// <param name="hWnd">ウィンドウハンドル</param>
+            /// <param name="dwAttribute">ウィンドウ属性</param>
+            /// <param name="rect">範囲の出力</param>
+            /// <param name="cbAttribute">はRECTのサイズ Marshal.SizeOf(typeof(RECT)) を指定</param>
+            /// <returns>戻り値が0なら成功、0以外ならエラー値</returns>
+            /// <see cref="https://gogowaten.hatenablog.com/entry/2020/11/17/004505"/>
+            [LibraryImport("dwmapi.dll")]
+            private static partial long DwmGetWindowAttribute(IntPtr hWnd, int dwAttribute, out RECT rect, int cbAttribute);
+
+            [StructLayout(LayoutKind.Sequential)]
+            private struct RECT
+            {
+                public int Left { get; set; }
+                public int Top { get; set; }
+                public int Right { get; set; }
+                public int Bottom { get; set; }
+            }
+
+            /// <summary>
+            /// コントロールからウィンドウのハンドルを取得
+            /// </summary>
+            /// <param name="hWnd">コントロール or ウィンドウのハンドル</param>
+            /// <returns>ウィンドウのハンドル</returns>
+            private static IntPtr GetWindowFromControl(IntPtr hWnd)
+            {
+                if ((GetWindowLongA(hWnd, GWL_STYLE) & WS_CHILD) == 0)
+                {
+                    return hWnd;
+                }
+                else
+                {
+                    return GetAncestor(hWnd, GA_ROOT);
+                }
+            }
+
+            /// <summary>
+            /// カーソルの位置にあるウィンドウの範囲を取得
+            /// </summary>
+            /// <returns>ウィンドウの範囲 取得できない場合はnull</returns>
+            public static Rect? GetWindowRectOnCursor()
+            {
+                // カーソルの位置を取得
+                if (!GetCursorPos(out var p)) return null;
+
+                // カーソルの位置にあるコントロールのハンドルを取得
+                IntPtr hWnd = WindowFromPoint(p);
+                if (hWnd == IntPtr.Zero) return null;
+
+                // コントロールからウィンドウのハンドルを取得
+                IntPtr hWindowWnd = GetWindowFromControl(hWnd);
+
+                // デスクトップは無視
+                if (hWindowWnd == GetShellWindow()) return null;
+                // タスクトレイは無視
+                if (hWindowWnd == FindWindowA("Shell_TrayWnd", null)) return null;
+
+                // ウィンドウの範囲を取得
+                if (DwmGetWindowAttribute(hWindowWnd, DWMWA_EXTENDED_FRAME_BOUNDS, out RECT rect, Marshal.SizeOf(typeof(RECT))) != 0) return null;
+
+                // ウィンドウの範囲をRectに変換して返す
+                return new Rect(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+            }
         }
     }
 
--- Prototype/SelectWindow.xaml.cs
+++ Prototype/SelectWindow.xaml.cs
@@ -23,160 +23,50 @@ namespace screen_draw_together.Prototype
     /// </summary>
     public partial class SelectWindow : Window
     {
-        [DllImport("user32.dll")]
-        static extern bool GetCursorPos(out System.Drawing.Point p);
-
-        [DllImport("user32.dll")]
-        static extern IntPtr WindowFromPoint(System.Drawing.Point p);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        static extern int GetWindowText(IntPtr hWnd, StringBuilder title, int size);
-
-        [DllImport("user32.dll")]
-        static extern bool GetWindowRect(IntPtr hwnd, out RECT rectangle);
-
-        [DllImport("user32.dll")]
-        static extern long GetWindowLong(IntPtr hWnd, int nIndex);
-
-        [DllImport("user32.dll")]
-        static extern IntPtr GetAncestor(IntPtr hWnd, uint gaFlags);
-
-        [DllImport("user32.dll")]
-        static extern IntPtr GetShellWindow();
-
-        [DllImport("user32.dll")]
-        static extern IntPtr FindWindow(string? lpClassName, string? lpWindowName);
-
-        const int WS_CHILD = 0x40000000;
-        const int GWL_STYLE = -16;
-        const int GA_ROOT = 2;
-        const int DWMWA_EXTENDED_FRAME_BOUNDS = 9;
-
-        /// <summary>
-        /// 引数のcbAttributeにこれを渡す
-        /// </summary>
-        /// <param name="hWnd">ウィンドウハンドル</param>
-        /// <param name="dwAttribute">ウィンドウ属性</param>
-        /// <param name="rect">範囲の出力</param>
-        /// <param name="cbAttribute">はRECTのサイズ Marshal.SizeOf(typeof(RECT)) を指定</param>
-        /// <returns>戻り値が0なら成功、0以外ならエラー値</returns>
-        /// <see cref="https://gogowaten.hatenablog.com/entry/2020/11/17/004505"/>
-        [DllImport("dwmapi.dll")]
-        static extern long DwmGetWindowAttribute(IntPtr hWnd, int dwAttribute, out RECT rect, int cbAttribute);
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-        {
-            public int Left { get; set; }
-            public int Top { get; set; }
-            public int Right { get; set; }
-            public int Bottom { get; set; }
-        }
-
-        // キャプチャー有効か
-        private bool _isCaptureEnable = false;
-        // タイマーのインスタンス
-        private DispatcherTimer _timer;
-
-
         // オーバーレイ
-        private AlphaOverlay _overlayWindow;
-        private RECT _rect;
-
+        private SelectBorder? _overlayWindow;
 
         public SelectWindow()
         {
             InitializeComponent();
-            SetupTimer();
-
-            _overlayWindow = new AlphaOverlay();
-            _overlayWindow.Show();
         }
 
-        // タイマーを設定する
-        private void SetupTimer()
+        private void CaptureWindowButton_Click(object sender, RoutedEventArgs e)
         {
-            // タイマーのインスタンスを生成
-            _timer = new DispatcherTimer();
-            // インターバルを設定
-            _timer.Interval = TimeSpan.FromMilliseconds(10);
-            // タイマーメソッドを設定
-            _timer.Tick += timer_Tick;
-
-            // 画面が閉じられるときに、タイマーを停止
-            this.Closing += (sender, e) => _timer.Stop();
-        }
-
-        /// <summary>
-        /// コントロールからウィンドウのハンドルを取得
-        /// </summary>
-        /// <param name="hWnd">コントロール or ウィンドウのハンドル</param>
-        /// <returns>ウィンドウのハンドル</returns>
-        private IntPtr GetWindowFromControl(IntPtr hWnd)
-        {
-            if ((GetWindowLong(hWnd, GWL_STYLE) & WS_CHILD) == 0)
+            if (_overlayWindow != null)
             {
-                return hWnd;
+                // ウィンドウを閉じる
+                _overlayWindow.Close();
+                _overlayWindow = null;
+                // ステート設定
+                Resources["CaptureWindowState"] = "OFF";
             }
             else
             {
-                return GetAncestor(hWnd, GA_ROOT);
-            }
-        }
-
-        private void timer_Tick(object? sender, EventArgs e)
-        {
-            if (GetCursorPos(out var p))
-            {
-                IntPtr hWnd = WindowFromPoint(p);
-                if (hWnd != IntPtr.Zero)
+                // ウィンドウを作成
+                _overlayWindow = new SelectBorder();
+                _overlayWindow.OnRectConfirmed += (rect) =>
                 {
-                    IntPtr hWindowWnd = GetWindowFromControl(hWnd);
+                    // ウィンドウを閉じる
+                    _overlayWindow?.Close();
+                    _overlayWindow = null;
+                    // ステート設定
+                    Resources["CaptureWindowState"] = "OFF";
 
-                    // デスクトップは無視
-                    if (hWindowWnd == GetShellWindow()) return;
-                    // タスクトレイは無視
-                    if (hWindowWnd == FindWindow("Shell_TrayWnd", null)) return;
-
-                    //if (GetWindowRect(hWnd, out var rect))
-                    if (DwmGetWindowAttribute(hWindowWnd, DWMWA_EXTENDED_FRAME_BOUNDS, out RECT rect, Marshal.SizeOf(typeof(RECT))) == 0)
+                    // ウィンドウの範囲を表示
+                    if (rect != null)
                     {
-                        // 同じの場合更新しない
-                        if (rect.Left == _rect.Left && rect.Top == _rect.Top && rect.Right == _rect.Right && rect.Bottom == _rect.Bottom)
-                        {
-                            return;
-                        }
-                        _rect = rect;
-
-                        captureWindowText.Content = $"RECT: Left:{rect.Left}, Top:{rect.Top}, Right:{rect.Right}, Bottom:{rect.Bottom}";
-                        _overlayWindow.Left = rect.Left;
-                        _overlayWindow.Top = rect.Top;
-                        _overlayWindow.Width = rect.Right - rect.Left;
-                        _overlayWindow.Height = rect.Bottom - rect.Top;
-                        return;
+                        var r = rect.Value;
+                        CaptureWindowText.Content = $"RECT: Left:{r.Left}, Top:{r.Top}, Right:{r.Right}, Bottom:{r.Bottom}";
                     }
-                }
-                captureWindowText.Content = "Not found";
-            }
-        }
-
-        private void captureWindowButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (_isCaptureEnable)
-            {
-                // タイマー停止
-                _timer.Stop();
+                    else
+                    {
+                        CaptureWindowText.Content = "Not found";
+                    }
+                };
+                _overlayWindow.Show();
                 // ステート設定
-                Resources["captureWindowState"] = "OFF";
-                _isCaptureEnable = false;
-            }
-            else
-            {
-                // タイマーを開始
-                _timer.Start();
-                // ステート設定
-                Resources["captureWindowState"] = "ON";
-                _isCaptureEnable = true;
+                Resources["CaptureWindowState"] = "ON";
             }
         }
     }
