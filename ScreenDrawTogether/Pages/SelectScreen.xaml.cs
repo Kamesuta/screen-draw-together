6c0ba417a39b63f868568945bab74b8c018092df
Kamesuta <kamesuta@gmail.com>
2023-09-22 14:10:14 +0900

    メニュー作成

---
+++ ScreenDrawTogether/Pages/SelectScreen.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ScreenDrawTogether.Prototype;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScreenDrawTogether.Pages
+{
+    /// <summary>
+    /// SelectScreen.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SelectScreen : Page
+    {
+        public SelectScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectMonitorButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
